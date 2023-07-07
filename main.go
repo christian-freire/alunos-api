c71d738fa105e6762edb6c5ba81e56036282bfe6
Christian Evangelista Freire <74272121+christian-freire@users.noreply.github.com>
2023-07-07 17:41:22 -0300

    [feat] Project initial modularization

--- main.go
+++ main.go
@@ -1,16 +1,7 @@
 package main
 
-import "github.com/gin-gonic/gin"
-
-func ExibeTodosAlunos(c *gin.Context) {
-	c.JSON(200, gin.H{
-		"id":   "1",
-		"nome": "Chris",
-	})
-}
+import "crud-api/routes"
 
 func main() {
-	r := gin.Default()
-	r.GET("/alunos", ExibeTodosAlunos)
-	r.Run()
+	routes.HandleRequests()
 }
---
+++ routes/routes.go
@@ -0,0 +1,13 @@
+package routes
+
+import (
+	"crud-api/controllers"
+
+	"github.com/gin-gonic/gin"
+)
+
+func HandleRequests() {
+	r := gin.Default()
+	r.GET("/alunos", controllers.ExibeTodosAlunos)
+	r.Run()
+}
